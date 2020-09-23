e0c3cd7ea4e71c970b1b765e574c0424269f6752
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-09-23 18:11:23 +0200

    Remove duplicate

--- measurement/src/main/java/de/peass/utils/RunCommandWriterSearchCause.java
+++ measurement/src/main/java/de/peass/utils/RunCommandWriterSearchCause.java
@@ -13,20 +13,7 @@ public class RunCommandWriterSearchCause extends RunCommandWriter {
 
    @Override
    public void createSingleMethodCommand(final int versionIndex, final String endversion, final String testcaseName) {
-      goal.println("java -cp distribution/target/peass-distribution-0.1-SNAPSHOT.jar " + RootCauseAnalysis.class.getCanonicalName() + " "
-            + "-test " + testcaseName + " "
-            + "-warmup 1000 "
-            + "-iterations 1000 "
-            + "-repetitions 100 "
-            + "-vms 100 "
-            + "-timeout 10 "
-            + "-type1error 0.2 "
-            + "-type2error 0.1 "
-            + "-version " + endversion + " "
-            + "-executionfile $PEASS_REPOS/dependencies-final/execute_" + name + ".json "
-            + "-folder ../../projekte/" + name + "/ "
-            + "-dependencyfile $PEASS_REPOS/dependencies-final/deps_" + name + ".json &> measurement_" + endversion.substring(0, 6) + "_" + testcaseName
-            + ".txt");
+      createSingleMethodCommand(versionIndex, endversion, testcaseName, 1000, 10000, 10000, 100);
    }
 
    public void createSingleMethodCommand(final int versionIndex, final String endversion, final String testcaseName, int warmup, int iterations, int repetitions, int vms) {
@@ -42,7 +29,7 @@ public class RunCommandWriterSearchCause extends RunCommandWriter {
             + "-type2error 0.1 "
             + "-version " + endversion + " "
             + "-executionfile $PEASS_REPOS/dependencies-final/execute_" + name + ".json "
-            + "-folder ../../projekte/" + name + "/ "
+            + "-folder ../projects/" + name + "/ "
             + "-dependencyfile $PEASS_REPOS/dependencies-final/deps_" + name + ".json &> measurement_" + endversion.substring(0, 6) + "_" + testcaseName
             + ".txt");
    }
