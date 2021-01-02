86e5b161ac4a6207f3ea626c7a0b8e85dc874983
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-01-02 18:47:35 +0100

    React on zoom in iterations

--- analysis/src/main/resources/visualization/jsGraphSource.js
+++ analysis/src/main/resources/visualization/jsGraphSource.js
@@ -1,40 +1,4 @@
 
-function getVmGraph(currentArray) {
-  var xvals = [], yvals = [];  
-  var n = 0;
-  for (i = 0; i < currentArray.length; i++){
-  	xvals[i] = n + currentArray[i].n/2;
-  	yvals[i] = currentArray[i].mean;
-  	n+=currentArray[i].n;
-  }
-  var data = {
-  	x: xvals,
-  	y: yvals,
-  	type: 'scatter',
-  	label: "test"
-  };
-  return data;
-}
-
-var currentNode;
-
-function plotVMGraph(divName, node, ids, idsPredecessor){
-  var data = [];
-  console.log(ids.length);
-  for (id = 0; id < ids.length; id++){
-  	data[id] = getVmGraph(node.vmValues.values[id]);
-  }
-  for (id = 0; id < idsPredecessor.length; id++){
-  	data[id + ids.length] = getVmGraph(node.vmValuesPredecessor.values[id]);
-  }
-  
-  var layout = {title: { text: "VM-wise Iteration Durations"},
-			xaxis: { title: { text: "Iteration"} },
-			yaxis: { title: { text: "Duration / &#x00B5;s"} }
-		  };
-  Plotly.newPlot(divName, data, layout);
-}
-
 function plotOverallHistogram(divName, node){
   var version = {
     x: node.values,
--- analysis/src/main/resources/visualization/peass-dashboard-start.js
+++ analysis/src/main/resources/visualization/peass-dashboard-start.js
@@ -1,3 +1,48 @@
+function getVmGraph(currentArray) {
+  var xvals = [], yvals = [];  
+  var n = 0;
+  for (i = 0; i < currentArray.length; i++){
+  	xvals[i] = n + currentArray[i].n/2;
+  	yvals[i] = currentArray[i].mean;
+  	n+=currentArray[i].n;
+  }
+  var data = {
+  	x: xvals,
+  	y: yvals,
+  	type: 'scatter',
+  	mode: 'lines+markers',
+  	marker: {size: 3}
+  };
+  return data;
+}
+
+function plotVMGraph(divName, node, ids, idsPredecessor){
+  var data = [];
+  console.log(ids.length);
+  for (id = 0; id < ids.length; id++){
+  	data[id] = getVmGraph(node.vmValues.values[id]);
+  }
+  for (id = 0; id < idsPredecessor.length; id++){
+  	data[id + ids.length] = getVmGraph(node.vmValuesPredecessor.values[id]);
+  }
+  
+  var layout = {title: { text: "VM-wise Iteration Durations"},
+			xaxis: { title: { text: "Iteration"} },
+			yaxis: { title: { text: "Duration / &#x00B5;s"},
+			mode: "lines+markers" }
+		  };
+  var graphDiv = document.getElementById(divName);
+  Plotly.newPlot(graphDiv, data, layout);
+  graphDiv.on('plotly_relayout', function(event){
+    console.log(event);
+    var xstart=event["xaxis.range[0]"];
+    var xend=event["xaxis.range[1]"];
+    visualizeHistogram(xstart, xend);
+    
+});
+}
+
+
 function addOptionSelectbox(divName, id, values){
 	var optionString='<select id="'+id+'" multiple style="height: 100%">';
 	for (var key of Object.keys(values)){
@@ -7,9 +52,6 @@ function addOptionSelectbox(divName, id, values){
 
 	document.getElementById(divName).innerHTML=optionString;
 }
-
-
-
 
 function visualizeGraph(divId, selectId){
 	var ids = [];
@@ -26,7 +68,7 @@ function visualizeGraph(divId, selectId){
 	}
 }
 
-function getAverage(divId, vmValues){
+function getAverage(divId, vmValues, start, end){
 	var averages = [];
  	var select = document.getElementById(divId);
  	var selectedOptions = $('#'+divId+' :selected');
@@ -34,19 +76,25 @@ function getAverage(divId, vmValues){
  	  var vmId = select.options[i].value;
  	  var iteration = vmValues.values[vmId];
  	  var mean = 0;
- 	  var count =0;
+ 	  var count =0, addedCount = 0;
  	  for (iterationIndex = 0; iterationIndex < iteration.length; iterationIndex++) {
- 	    mean+=iteration[iterationIndex].mean*iteration[iterationIndex].n;
- 	    count+=iteration[iterationIndex].n;
+	    count+=iteration[iterationIndex].n;
+ 	    if (start == null && end == null){
+ 	      mean+=iteration[iterationIndex].mean*iteration[iterationIndex].n;
+ 	      addedCount+=iteration[iterationIndex].n;
+ 	    } else if (count > start && count < end){
+ 	      mean+=iteration[iterationIndex].mean*iteration[iterationIndex].n;
+ 	      addedCount+=iteration[iterationIndex].n;
+ 	    }
  	  }
- 	  averages[i] = mean/count;
+ 	  averages[i] = mean/addedCount;
  	}
  	return averages;
 }
 
-function visualizeHistogram(){
-	var averagesPredecessor = getAverage("predecessorSelect", currentNode.vmValuesPredecessor);
- 	var averagesCurrent = getAverage("predecessorSelect", currentNode.vmValues);
+function visualizeHistogram(start, end){
+	var averagesPredecessor = getAverage("predecessorSelect", currentNode.vmValuesPredecessor, start, end);
+ 	var averagesCurrent = getAverage("predecessorSelect", currentNode.vmValues, start, end);
  	
  	var version = {
 	    x: averagesCurrent,
