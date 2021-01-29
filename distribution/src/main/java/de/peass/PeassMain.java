3d180eb2de6426fd6c48e30f88f1bb64831c1d47
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-01-29 17:10:23 +0100

    Fix autocompletion - speedup build

--- distribution/src/main/java/de/peass/PeassMain.java
+++ distribution/src/main/java/de/peass/PeassMain.java
@@ -20,9 +20,9 @@ import picocli.CommandLine.Command;
       VisualizeRCA.class, 
       ContinuousExecutionStarter.class,
       FindMissingExecutions.class}, synopsisSubcommandLabel = "COMMAND")
-public class PeASSMain implements Callable<Void> {
+public class PeassMain implements Callable<Void> {
    public static void main(final String[] args) {
-      final CommandLine line = new CommandLine(new PeASSMain());
+      final CommandLine line = new CommandLine(new PeassMain());
       if (args.length != 0) {
          System.exit(line.execute(args));
       } else {
