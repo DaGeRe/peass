57b40b247123556a42004eb4d7454769758d3051
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-02-03 13:22:10 +0100

    Fix test PowerMock configuration

--- measurement/src/test/java/de/peass/measurement/analysis/TestDependencyTester.java
+++ measurement/src/test/java/de/peass/measurement/analysis/TestDependencyTester.java
@@ -1,47 +1,34 @@
 package de.peass.measurement.analysis;
 
 import java.io.File;
-import java.io.IOException;
 
 import javax.xml.bind.JAXBException;
 
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import de.dagere.kopeme.datacollection.TimeDataCollector;
 import de.dagere.kopeme.datastorage.XMLDataLoader;
-import de.dagere.kopeme.datastorage.XMLDataStorer;
 import de.dagere.kopeme.generated.Kopemedata;
-import de.dagere.kopeme.generated.Result;
-import de.dagere.kopeme.generated.Result.Fulldata;
-import de.dagere.kopeme.generated.Result.Fulldata.Value;
 import de.dagere.kopeme.generated.TestcaseType.Datacollector;
 import de.dagere.kopeme.generated.TestcaseType.Datacollector.Chunk;
 import de.peass.dependency.ExecutorCreator;
 import de.peass.dependency.PeASSFolders;
 import de.peass.dependency.analysis.data.TestCase;
 import de.peass.dependency.execution.MeasurementConfiguration;
-import de.peass.dependency.execution.TestExecutor;
 import de.peass.dependencyprocessors.DependencyTester;
 import de.peass.measurement.MavenTestExecutorMocker;
 import de.peass.measurement.rca.helper.VCSTestUtils;
-import de.peass.testtransformation.JUnitTestTransformer;
 import de.peass.vcs.VersionControlSystem;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ VersionControlSystem.class, ExecutorCreator.class })
-@PowerMockIgnore("javax.management.*")
+@PowerMockIgnore({ "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*", "org.w3c.dom.*" })
 public class TestDependencyTester {
 
    public static final TestCase EXAMPLE_TESTCASE = new TestCase("de.peass.MyTest", "test");
