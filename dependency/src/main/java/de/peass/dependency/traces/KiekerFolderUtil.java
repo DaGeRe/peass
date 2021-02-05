6144b4888eeb875dedec5aaafacbb7abb157ab91
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-02-05 14:42:06 +0100

    Try to print log file if it was not found as expected

--- dependency/src/main/java/de/peass/dependency/traces/KiekerFolderUtil.java
+++ dependency/src/main/java/de/peass/dependency/traces/KiekerFolderUtil.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
@@ -30,7 +28,7 @@ public class KiekerFolderUtil {
          }
       });
       if (listFiles == null) {
-         tryToWriteLastLog(testcase, resultsFolder);
+         new ErrorLogWriter(testcase, resultsFolder).tryToWriteLastLog();
          throw new ViewNotFoundException("Probably project not running - Result folder: " + Arrays.toString(listFiles) + " ("
                + (listFiles != null ? listFiles.length : "null") + ") in " + projectResultFolder.getAbsolutePath() + " should exist!");
       }
@@ -50,51 +48,7 @@ public class KiekerFolderUtil {
       }
    }
 
-   private static void tryToWriteLastLog(final TestCase testcase, final File resultsFolder) {
-      File logFolder = new File(resultsFolder, "../../logs/");
-      System.out.println("Searching in" + logFolder.getAbsolutePath());
-      if (logFolder.exists()) {
-         File logFolderChild = logFolder.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(final File pathname) {
-               return pathname.isDirectory();
-            }
-         })[0];
-         System.out.println("First folder: " + logFolderChild.getAbsolutePath());
-         File txtFile = new File(logFolderChild, "log_" + testcase.getClazz() + "/" + testcase.getMethod());
-         System.out.println("Trying " + txtFile.getAbsolutePath());
-         if (txtFile.exists()) {
-            try {
-               System.out.println(FileUtils.readFileToString(txtFile, StandardCharsets.UTF_8));
-            } catch (IOException e) {
-               e.printStackTrace();
-            }
-         }
-      } else {
-         File testingFolder = new File(resultsFolder, "../");
-         int index = 0;
-         while (!testingFolder.exists() && index < 10) {
-            System.out.println("Folder " + testingFolder.getAbsolutePath() + " did not exist");
-            testingFolder = new File(testingFolder.getAbsolutePath(), "..");
-            index++;
-         }
-         if (testingFolder.exists()) {
-            System.out.println("Files in " + testingFolder.getAbsolutePath());
-            for (File file : testingFolder.listFiles()) {
-               System.out.println(file.getAbsolutePath());
-            }
-         }else {
-            System.out.println("Did no succeed searching in parent folders; start to search from root");
-            String[] parts = resultsFolder.getAbsolutePath().split(File.separator);
-            File userFolder = new File(File.separator + parts[0] + File.separator + parts[1]);
-            System.out.println(userFolder.getAbsolutePath() + " " + userFolder.exists());
-            for (int i = 2; i < parts.length; i++) {
-               userFolder = new File(userFolder, parts[i]);
-               System.out.println(userFolder.getAbsolutePath() + " " + userFolder.exists());
-            }
-         }
-      }
-   }
+   
 
    public static File getKiekerTraceFolder(final File kiekerResultFolder, final TestCase testcase) {
       File methodResult = new File(kiekerResultFolder, testcase.getMethod());
