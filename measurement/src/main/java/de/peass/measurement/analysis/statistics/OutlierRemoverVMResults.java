c66a297d37484a4d81cc5ef8348442697f164fdd
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-02-10 12:12:09 +0100

    Only remove outliers if more than 3 values are present

--- measurement/src/main/java/de/peass/measurement/analysis/statistics/OutlierRemoverVMResults.java
+++ measurement/src/main/java/de/peass/measurement/analysis/statistics/OutlierRemoverVMResults.java
@@ -6,33 +6,36 @@ import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import de.peass.measurement.rca.data.CallTreeNode;
 import de.peass.measurement.rca.data.OneVMResult;
 
 public class OutlierRemoverVMResults {
 
    private static final Logger LOG = LogManager.getLogger(OutlierRemoverVMResults.class);
 
-   public static void getValuesWithoutOutliers(List<OneVMResult> results, SummaryStatistics statistics) {
-      SummaryStatistics fullStatistic = new SummaryStatistics();
-      for (final OneVMResult result : results) {
-         final double average = result.getAverage();
-         fullStatistic.addValue(average);
-      }
-
-      double min = fullStatistic.getMean() - OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
-      double max = fullStatistic.getMean() + OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
-
-      LOG.debug("Removing outliers between {} and {} - Old vm count: {}", min, max, results.size());
-      for (final OneVMResult result : results) {
-         final double average = result.getAverage();
-         if (average >= min && average <= max) {
-            statistics.addValue(average);
-            LOG.trace("Adding value: {}", average);
-         } else {
-            LOG.debug("Not adding outlier: {}", average);
+   public static void getValuesWithoutOutliers(final List<OneVMResult> results, final SummaryStatistics statistics) {
+      if (results.size() > 3) {
+         SummaryStatistics fullStatistic = new SummaryStatistics();
+         for (final OneVMResult result : results) {
+            final double average = result.getAverage();
+            fullStatistic.addValue(average);
          }
+
+         double min = fullStatistic.getMean() - OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
+         double max = fullStatistic.getMean() + OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
+
+         LOG.debug("Removing outliers between {} and {} - Old vm count: {}", min, max, results.size());
+         for (final OneVMResult result : results) {
+            final double average = result.getAverage();
+            if (average >= min && average <= max) {
+               statistics.addValue(average);
+               LOG.trace("Adding value: {}", average);
+            } else {
+               LOG.debug("Not adding outlier: {}", average);
+            }
+         }
+         LOG.debug("VM count after removal: {}", statistics.getN());
+      } else {
+         LOG.debug("Skipping outlier removal because of VM count below 2");
       }
-      LOG.debug("VM count after removal: {}", statistics.getN());
    }
 }
