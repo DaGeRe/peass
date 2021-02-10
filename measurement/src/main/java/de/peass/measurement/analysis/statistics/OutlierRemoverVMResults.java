5e8d3a0f06b77e5d05b1181b6c66944c5715ce94
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-02-10 12:22:17 +0100

    Fix outlier removal

--- measurement/src/main/java/de/peass/measurement/analysis/statistics/OutlierRemoverVMResults.java
+++ measurement/src/main/java/de/peass/measurement/analysis/statistics/OutlierRemoverVMResults.java
@@ -15,27 +15,36 @@ public class OutlierRemoverVMResults {
    public static void getValuesWithoutOutliers(final List<OneVMResult> results, final SummaryStatistics statistics) {
       if (results.size() > 3) {
          SummaryStatistics fullStatistic = new SummaryStatistics();
-         for (final OneVMResult result : results) {
-            final double average = result.getAverage();
-            fullStatistic.addValue(average);
-         }
+         addAll(results, fullStatistic);
 
          double min = fullStatistic.getMean() - OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
          double max = fullStatistic.getMean() + OutlierRemover.Z_SCORE * fullStatistic.getStandardDeviation();
 
          LOG.debug("Removing outliers between {} and {} - Old vm count: {}", min, max, results.size());
-         for (final OneVMResult result : results) {
-            final double average = result.getAverage();
-            if (average >= min && average <= max) {
-               statistics.addValue(average);
-               LOG.trace("Adding value: {}", average);
-            } else {
-               LOG.debug("Not adding outlier: {}", average);
-            }
-         }
+         addNonOutlier(results, statistics, min, max);
          LOG.debug("VM count after removal: {}", statistics.getN());
       } else {
          LOG.debug("Skipping outlier removal because of VM count below 2");
+         addAll(results, statistics);
+      }
+   }
+
+   private static void addNonOutlier(final List<OneVMResult> results, final SummaryStatistics statistics, double min, double max) {
+      for (final OneVMResult result : results) {
+         final double average = result.getAverage();
+         if (average >= min && average <= max) {
+            statistics.addValue(average);
+            LOG.trace("Adding value: {}", average);
+         } else {
+            LOG.debug("Not adding outlier: {}", average);
+         }
+      }
+   }
+
+   private static void addAll(final List<OneVMResult> results, final SummaryStatistics statistics) {
+      for (final OneVMResult result : results) {
+         final double average = result.getAverage();
+         statistics.addValue(average);
       }
    }
 }
