7a8e6a5b2c9a45294f240706de9fd6bbc536586c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-03-25 19:38:53 +0100

    Fix test, superclass dependencies were missing because constructor of superclass was not instrumented

--- dependency/src/main/java/net/kieker/sourceinstrumentation/InstrumentationConfiguration.java
+++ dependency/src/main/java/net/kieker/sourceinstrumentation/InstrumentationConfiguration.java
@@ -19,7 +19,7 @@ public class InstrumentationConfiguration {
       this.sample = sample;
       this.includedPatterns = includedPatterns;
       this.enableAdaptiveMonitoring = enableAdaptiveMonitoring;
-      this.createDefaultConstructor = false;
+      this.createDefaultConstructor = true;
       this.enableDeactivation = enableDecativation;
    }
 
