052ce6b5469bedf7ba45e19f33ae3d9b8ca23a30
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-04 15:29:33 +0200

    Ignore uncompleted test

--- dependency/src/test/java/de/dagere/peass/jmh/JmhTest.java
+++ dependency/src/test/java/de/dagere/peass/jmh/JmhTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -31,6 +32,7 @@ import de.dagere.peass.dependencytests.TraceGettingIT;
 import de.dagere.peass.dependencytests.helper.FakeFileIterator;
 import de.dagere.peass.vcs.GitUtils;
 
+@Ignore
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(GitUtils.class)
 @PowerMockIgnore({ "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*", "org.w3c.dom.*" })
