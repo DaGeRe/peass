c67e2e23de34fab999870e7cf15057611b3a1ec2
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-07 14:55:40 +0200

    Implement clazzname conversion

--- dependency/src/main/java/de/dagere/peass/dependency/jmh/JmhResultMover.java
+++ dependency/src/main/java/de/dagere/peass/dependency/jmh/JmhResultMover.java
@@ -2,10 +2,19 @@ package de.dagere.peass.dependency.jmh;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import de.dagere.kopeme.datastorage.XMLDataStorer;
+import de.dagere.kopeme.generated.Kopemedata;
+import de.dagere.kopeme.generated.Kopemedata.Testcases;
+import de.dagere.kopeme.generated.TestcaseType;
 import de.dagere.peass.dependency.PeASSFolders;
 import de.dagere.peass.dependency.analysis.data.TestCase;
 import de.dagere.peass.dependency.traces.KiekerFolderUtil;
+import de.dagere.peass.utils.Constants;
 
 public class JmhResultMover {
 
@@ -36,11 +45,31 @@ public class JmhResultMover {
       kiekerTimeFolder.mkdirs();
 
       final File expectedKoPeMeFile = new File(clazzResultFolder, testcase.getMethod() + ".xml");
-      sourceJsonResultFile.renameTo(expectedKoPeMeFile);
+      convertToXMLData(sourceJsonResultFile, expectedKoPeMeFile);
       
       if (sourceResultFolder != null) {
          final File kiekerSubfolder = new File(kiekerTimeFolder, sourceResultFolder.getName());
          sourceResultFolder.renameTo(kiekerSubfolder);
+      }
+   }
+
+   private void convertToXMLData(final File sourceJsonResultFile, final File expectedKoPeMeFile) {
+      try {
+         ArrayNode benchmarks = (ArrayNode) Constants.OBJECTMAPPER.readTree(sourceJsonResultFile);
+         JsonNode benchmark = benchmarks.get(0);
+         String name = benchmark.get("benchmark").asText();
+         JsonNode primaryMetric = benchmark.get("primaryMetric");
+         ArrayNode rawData = (ArrayNode) primaryMetric.get("rawData");
+         
+         Kopemedata transformed = new Kopemedata();
+         transformed.setTestcases(new Testcases());
+         TestcaseType testclazz = new TestcaseType();
+         transformed.getTestcases().getTestcase().add(testclazz);
+         testclazz.setName(name.substring(0, name.lastIndexOf('.')));
+         
+         XMLDataStorer.storeData(expectedKoPeMeFile, transformed);
+      } catch (IOException e) {
+         throw new RuntimeException(e);
       }
    }
 }
--- dependency/src/test/java/de/dagere/peass/jmh/JmhIterationTest.java
+++ dependency/src/test/java/de/dagere/peass/jmh/JmhIterationTest.java
@@ -8,14 +8,15 @@ import javax.xml.bind.JAXBException;
 
 import org.apache.commons.io.FileUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.github.javaparser.ParseException;
 
 import de.dagere.kopeme.datastorage.XMLDataLoader;
 import de.dagere.kopeme.generated.Kopemedata;
+import de.dagere.kopeme.generated.TestcaseType;
 import de.dagere.peass.TestConstants;
 import de.dagere.peass.TestUtil;
 import de.dagere.peass.config.MeasurementConfiguration;
@@ -27,7 +28,6 @@ import de.dagere.peass.dependency.execution.TestExecutor;
 import de.dagere.peass.dependency.jmh.JMHTestTransformer;
 import de.dagere.peass.dependencyprocessors.ViewNotFoundException;
 
-@Ignore
 public class JmhIterationTest {
    
    @Before
@@ -52,6 +52,9 @@ public class JmhIterationTest {
       
       File clazzFolder = folders.findTempClazzFolder(testcase).get(0);
       Kopemedata data = XMLDataLoader.loadData(new File(clazzFolder, testcase.getMethod() + ".xml"));
+      
+      TestcaseType testcaseData = data.getTestcases().getTestcase().get(0);
+      Assert.assertEquals("de.dagere.peass.ExampleBenchmark", testcaseData.getName());
    }
 
 }
