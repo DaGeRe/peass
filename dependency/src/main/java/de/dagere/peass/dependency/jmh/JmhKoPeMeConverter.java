3535678ef969e2974eb1f8f1758991dfed9b1df7
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-14 17:24:26 +0200

    Avoid collision because of name

--- dependency/src/main/java/de/dagere/peass/dependency/jmh/JmhKoPeMeConverter.java
+++ dependency/src/main/java/de/dagere/peass/dependency/jmh/JmhKoPeMeConverter.java
@@ -3,29 +3,16 @@ package de.dagere.peass.dependency.jmh;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.bind.JAXBException;
 
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-import de.dagere.kopeme.datacollection.TimeDataCollectorNoGC;
-import de.dagere.kopeme.datastorage.XMLDataLoader;
 import de.dagere.kopeme.datastorage.XMLDataStorer;
-import de.dagere.kopeme.generated.Kopemedata;
-import de.dagere.kopeme.generated.Kopemedata.Testcases;
-import de.dagere.kopeme.generated.Result;
-import de.dagere.kopeme.generated.Result.Fulldata;
-import de.dagere.kopeme.generated.Result.Fulldata.Value;
-import de.dagere.kopeme.generated.Result.Params;
-import de.dagere.kopeme.generated.Result.Params.Param;
-import de.dagere.kopeme.generated.TestcaseType;
-import de.dagere.kopeme.generated.TestcaseType.Datacollector;
 import de.dagere.peass.config.MeasurementConfiguration;
+import de.dagere.peass.dependency.analysis.data.TestCase;
 import de.dagere.peass.utils.Constants;
 
 /**
@@ -52,42 +39,20 @@ public class JmhKoPeMeConverter {
             for (JsonNode benchmark : benchmarks) {
 
                final String name = benchmark.get("benchmark").asText();
+               String clazzName = name.substring(0, name.lastIndexOf('.'));
                String benchmarkMethodName = name.substring(name.lastIndexOf('.') + 1);
+
+               TestCase testcase = new TestCase(clazzName, benchmarkMethodName);
 
                JsonNode primaryMetric = benchmark.get("primaryMetric");
                String scoreUnit = primaryMetric.get("scoreUnit").asText();
                ArrayNode rawData = (ArrayNode) primaryMetric.get("rawData");
 
-               File koPeMeFile = new File(clazzResultFolder, benchmarkMethodName + ".xml");
-               results.add(koPeMeFile);
-               Kopemedata transformed;
-               Datacollector timeCollector;
-               if (koPeMeFile.exists()) {
-                  transformed = XMLDataLoader.loadData(koPeMeFile);
-                  timeCollector = transformed.getTestcases().getTestcase().get(0).getDatacollector().get(0);
-               } else {
-                  transformed = new Kopemedata();
-                  Testcases testcases = new Testcases();
-                  testcases.setClazz(name.substring(0, name.lastIndexOf('.')));
-                  transformed.setTestcases(testcases);
-                  TestcaseType testclazz = new TestcaseType();
-                  transformed.getTestcases().getTestcase().add(testclazz);
-                  testclazz.setName(name.substring(name.lastIndexOf('.') + 1));
-                  timeCollector = new Datacollector();
-                  timeCollector.setName(TimeDataCollectorNoGC.class.getName());
-                  testclazz.getDatacollector().add(timeCollector);
-               }
+               JmhBenchmarkConverter converter = new JmhBenchmarkConverter(testcase, clazzResultFolder, measurementConfig);
+               converter.convertData(rawData, benchmark, scoreUnit);
 
-               for (JsonNode vmExecution : rawData) {
-                  Result result = buildResult(vmExecution, scoreUnit);
-                  JsonNode params = benchmark.get("params");
-                  if (params != null) {
-                     setParamMap(result, params);
-                  }
-                  timeCollector.getResult().add(result);
-               }
-
-               XMLDataStorer.storeData(koPeMeFile, transformed);
+               XMLDataStorer.storeData(converter.getKoPeMeFile(), converter.getTransformed());
+               results.add(converter.getKoPeMeFile());
             }
          }
       } catch (IOException | JAXBException e) {
@@ -96,50 +61,5 @@ public class JmhKoPeMeConverter {
       return results;
    }
 
-   private void setParamMap(final Result result, final JsonNode params) {
-      result.setParams(new Params());
-      for (Iterator<String> fieldIterator = params.fieldNames(); fieldIterator.hasNext();) {
-         final String field = fieldIterator.next();
-         final String value = params.get(field).asText();
-         final Param param = new Param();
-         param.setKey(field);
-         param.setValue(value);
-         result.getParams().getParam().add(param);
-      }
-   }
-
-   private Result buildResult(final JsonNode vmExecution, final String scoreUnit) {
-      Result result = new Result();
-      Fulldata fulldata = buildFulldata(vmExecution, scoreUnit);
-      result.setFulldata(fulldata);
-
-      DescriptiveStatistics statistics = new DescriptiveStatistics();
-      result.getFulldata().getValue().forEach(value -> statistics.addValue(value.getValue()));
-      result.setValue(statistics.getMean());
-      result.setDeviation(statistics.getStandardDeviation());
-      result.setIterations(result.getFulldata().getValue().size());
-
-      // Assume that warmup and repetitions took place as defined, since they are not recorded by jmh
-      result.setWarmup(measurementConfig.getWarmup());
-      result.setRepetitions(measurementConfig.getRepetitions());
-
-      return result;
-   }
-
-   private Fulldata buildFulldata(final JsonNode vmExecution, final String scoreUnit) {
-      Fulldata fulldata = new Fulldata();
-      for (JsonNode iteration : vmExecution) {
-         Value value = new Value();
-         long iterationDuration;
-         if (!scoreUnit.equals("ops/s")) {
-            iterationDuration = (long) (iteration.asDouble() * SECONDS_TO_NANOSECONDS);
-         } else {
-            iterationDuration = iteration.asLong();
-         }
-
-         value.setValue(iterationDuration);
-         fulldata.getValue().add(value);
-      }
-      return fulldata;
-   }
+   
 }
--- dependency/src/test/java/de/dagere/peass/jmh/TestResultConversion.java
+++ dependency/src/test/java/de/dagere/peass/jmh/TestResultConversion.java
@@ -46,8 +46,6 @@ public class TestResultConversion {
       Assert.assertEquals(result.getValue(), 1101075.0, 0.01);
       Assert.assertEquals(result.getFulldata().getValue().get(0).getValue(), 1101075);
    }
-
-   
    
    @Test
    public void testOriginalJmhConversion() throws JAXBException {
@@ -65,6 +63,24 @@ public class TestResultConversion {
       Result result = results.get(0);
       Assert.assertEquals(result.getValue(), 97.0, 0.01);
       Assert.assertEquals(result.getFulldata().getValue().get(0).getValue(), 96);
+   }
+   
+   @Test
+   public void testOriginalJmhConversionParams() throws JAXBException {
+      File jmhFile = new File(CONVERSION_FOLDER, "example-with-params.json");
+      
+      Set<File> resultFiles = convert(jmhFile);
+      
+      Assert.assertThat(resultFiles, Matchers.hasSize(1));
+      
+      File resultFile = resultFiles.iterator().next();
+      Kopemedata data = XMLDataLoader.loadData(resultFile);
+      
+      List<Result> results = data.getTestcases().getTestcase().get(0).getDatacollector().get(0).getResult();
+      Assert.assertThat(results, Matchers.hasSize(3));
+      Result result = results.get(0);
+      Assert.assertEquals(result.getParams().getParam().get(0).getKey(), "TEST_PARAM");
+      Assert.assertEquals(result.getParams().getParam().get(0).getValue(), "val1");
    }
    
    private Set<File> convert(final File jmhFile) {
