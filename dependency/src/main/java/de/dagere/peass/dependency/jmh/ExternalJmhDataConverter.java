5d76dd3fbdf912c5f25967dde41759ffd3f8c9c8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-14 16:08:31 +0200

    Merge same params to chunk

--- dependency/src/main/java/de/dagere/peass/dependency/jmh/ExternalJmhDataConverter.java
+++ dependency/src/main/java/de/dagere/peass/dependency/jmh/ExternalJmhDataConverter.java
@@ -1,10 +1,14 @@
 package de.dagere.peass.dependency.jmh;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.xml.bind.JAXBException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import de.dagere.kopeme.datastorage.XMLDataLoader;
 import de.dagere.kopeme.datastorage.XMLDataStorer;
@@ -18,40 +22,70 @@ import de.dagere.kopeme.generated.Versioninfo;
 import de.dagere.peass.config.MeasurementConfiguration;
 
 public class ExternalJmhDataConverter {
+
+   private static final Logger LOG = LogManager.getLogger(ExternalJmhDataConverter.class);
+
    public static void main(final String[] args) throws JAXBException {
+      Set<File> allResultFiles = new LinkedHashSet<>();
       for (String input : args) {
          File inputFile = new File(input);
          if (inputFile.isDirectory()) {
             for (File child : inputFile.listFiles()) {
                if (child.getName().endsWith(".json")) {
-                  convertFileNoData(child);
+                  Set<File> currentResultFiles = convertFileNoData(child);
+                  allResultFiles.addAll(currentResultFiles);
                }
             }
          } else {
-            convertFileNoData(inputFile);
+            Set<File> currentResultFiles = convertFileNoData(inputFile);
+            allResultFiles.addAll(currentResultFiles);
+         }
+      }
+
+      for (File resultFile : allResultFiles) {
+         LOG.debug("Handling " + resultFile);
+         Kopemedata data = XMLDataLoader.loadData(resultFile);
+         mergeDoubleChunks(data);
+         XMLDataStorer.storeData(resultFile, data);
+      }
+   }
+
+   private static void mergeDoubleChunks(final Kopemedata data) {
+      Datacollector datacollector = data.getTestcases().getTestcase().get(0).getDatacollector().get(0);
+      for (Iterator<Chunk> iterator = datacollector.getChunk().iterator(); iterator.hasNext();) {
+         Chunk currentChunk = iterator.next();
+         String paramString = paramsToString(currentChunk.getResult().get(0).getParams());
+         for (Iterator<Chunk> innerIterator = datacollector.getChunk().iterator(); innerIterator.hasNext();) {
+            Chunk innerChunk = innerIterator.next();
+            if (currentChunk != innerChunk) {
+               String innerParamString = paramsToString(innerChunk.getResult().get(0).getParams());
+               if (paramString.equals(innerParamString)) {
+                  LOG.debug("Removing " + innerParamString);
+                  iterator.remove();
+                  innerChunk.getResult().addAll(currentChunk.getResult());
+                  break;
+               }
+            }
          }
       }
    }
 
-   private static void convertFileNoData(final File child) throws JAXBException {
+   private static Set<File> convertFileNoData(final File child) throws JAXBException {
       JmhKoPeMeConverter converter = new JmhKoPeMeConverter(new MeasurementConfiguration(-1));
       Set<File> resultFiles = converter.convertToXMLData(child, child.getParentFile());
 
       String currentVersion = child.getName().substring(0, child.getName().length() - ".json".length()); // Remove .json
-      createChunk(resultFiles, currentVersion);
+      createChunks(resultFiles, currentVersion);
+
+      return resultFiles;
    }
 
-   private static void createChunk(final Set<File> resultFiles, final String currentVersion) throws JAXBException {
+   private static void createChunks(final Set<File> resultFiles, final String currentVersion) throws JAXBException {
       for (File resultFile : resultFiles) {
-         System.out.println("Hadnling " + resultFile);
+         LOG.info("Handling " + resultFile);
          Kopemedata data = XMLDataLoader.loadData(resultFile);
          Datacollector datacollector = data.getTestcases().getTestcase().get(0).getDatacollector().get(0);
-         Set<String> allParams = new LinkedHashSet<>();
-         for (Result result : datacollector.getResult()) {
-            Params params = result.getParams();
-            allParams.add(paramsToString(params));
-         }
-         System.out.println(allParams);
+         Set<String> allParams = getParams(datacollector);
          for (String params : allParams) {
             Chunk addedChunk = new Chunk();
             datacollector.getResult().forEach(result -> {
@@ -66,6 +100,16 @@ public class ExternalJmhDataConverter {
          datacollector.getResult().clear();
          XMLDataStorer.storeData(resultFile, data);
       }
+   }
+
+   private static Set<String> getParams(final Datacollector datacollector) {
+      Set<String> allParams = new LinkedHashSet<>();
+      for (Result result : datacollector.getResult()) {
+         Params params = result.getParams();
+         allParams.add(paramsToString(params));
+      }
+      LOG.info("Params: {}", allParams);
+      return allParams;
    }
 
    public static String paramsToString(final Params params) {
