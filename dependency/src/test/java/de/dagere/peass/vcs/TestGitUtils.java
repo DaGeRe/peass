e8088b8f49cf63c65719a1ca00806777c1df5c77
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-16 16:56:49 +0200

    Set identity for example git commits

--- dependency/src/test/java/de/dagere/peass/vcs/TestGitUtils.java
+++ dependency/src/test/java/de/dagere/peass/vcs/TestGitUtils.java
@@ -18,6 +18,9 @@ public class TestGitUtils {
    private static final String FEATURE_A = "feature-A";
    private static final String PEASS_TEST_MAIN_BRANCH = "peass-test-main";
    private final static File PROJECT_FOLDER = new File(TestConstants.CURRENT_FOLDER, "demo-git");
+   
+   private static final File exampleTextFile = new File(PROJECT_FOLDER, "file.txt");
+   private static final File secondTextFile = new File(PROJECT_FOLDER, "second.txt");
 
    @Before
    public void prepareProject() throws InterruptedException, IOException {
@@ -25,7 +28,7 @@ public class TestGitUtils {
       PROJECT_FOLDER.mkdirs();
       ProjectBuilderHelper.init(PROJECT_FOLDER);
 
-      File exampleTextFile = new File(PROJECT_FOLDER, "file.txt");
+      
       FileUtils.writeStringToFile(exampleTextFile, "Dummy", StandardCharsets.UTF_8);
       ProjectBuilderHelper.commit(PROJECT_FOLDER, "Dummy-version for avoiding branch clashes (default branch might be main or master)");
 
@@ -43,6 +46,7 @@ public class TestGitUtils {
       for (int i = 0; i < 3; i++) {
          createCommit(exampleTextFile, "A", i);
       }
+      ProjectBuilderHelper.checkout(PROJECT_FOLDER, PEASS_TEST_MAIN_BRANCH);
    }
 
    private void createCommit(final File exampleTextFile, final String prefix, final int i) throws IOException, InterruptedException {
@@ -52,26 +56,25 @@ public class TestGitUtils {
 
    @Test
    public void testBasicCommitGetting() throws InterruptedException, IOException {
-      ProjectBuilderHelper.checkout(PROJECT_FOLDER, PEASS_TEST_MAIN_BRANCH);
-      
       List<GitCommit> commitsAll = GitUtils.getCommits(PROJECT_FOLDER, true);
       Assert.assertEquals(commitsAll.size(), 7);
-      
+
       List<GitCommit> commits = GitUtils.getCommits(PROJECT_FOLDER, false);
       Assert.assertEquals(commits.size(), 4);
-      
+
       ProjectBuilderHelper.merge(PROJECT_FOLDER, FEATURE_A);
-      
+
       List<GitCommit> commitsMerged = GitUtils.getCommits(PROJECT_FOLDER, false);
       Assert.assertEquals(commitsMerged.size(), 7);
    }
 
-   // @Test
-   // public void testMergeCommits() {
-   // createMergeCommit(exampleTextFile, 6);
-   // createMergeCommit(exampleTextFile, 7);
-   // }
-   
+   @Test
+   public void testMergeCommits() throws InterruptedException, IOException {
+      FileUtils.writeStringToFile(secondTextFile, "Just some text", StandardCharsets.UTF_8);
+      createMergeCommit(exampleTextFile, 6);
+      createMergeCommit(exampleTextFile, 7);
+   }
+
    private void createMergeCommit(final File exampleTextFile, final int index) throws InterruptedException, IOException {
       createCommit(exampleTextFile, "", index);
 
