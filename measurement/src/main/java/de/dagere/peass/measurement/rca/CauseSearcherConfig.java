c01fdef574f87e66baf73ac5e35d6b703e7c3ea8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-06-03 10:20:55 +0200

    Check that level count is one or above

--- measurement/src/main/java/de/dagere/peass/measurement/rca/CauseSearcherConfig.java
+++ measurement/src/main/java/de/dagere/peass/measurement/rca/CauseSearcherConfig.java
@@ -13,16 +13,16 @@ import de.dagere.peass.dependency.analysis.data.TestCase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CauseSearcherConfig implements Serializable {
-   
+
    private static final long serialVersionUID = 5893295648840988829L;
-   
+
    private final TestCase testCase;
    private final boolean ignoreEOIs;
    private final boolean useAggregation;
    private final boolean splitAggregated;
    private final double minTime;
    private final boolean calibrationRun;
-   
+
    @JsonInclude(Include.NON_NULL)
    private File propertyFolder;
    private final RCAStrategy rcaStrategy;
@@ -52,18 +52,23 @@ public class CauseSearcherConfig implements Serializable {
       if (rcaStrategy != RCAStrategy.LEVELWISE && levels > 1) {
          throw new RuntimeException("If levels > 1, strategy must be LEVELWISE");
       }
+      if (levels < 1) {
+         // TODO It would be possible to refactor this to two strategies, UNTIL_SOURCE_CHANGE and LEVELWISE,
+         // where -1 means complete tree and 1 and above mean constant count of levels; than this check needs to be changed
+         throw new RuntimeException("Levels need to be 1 or above!");
+      }
    }
-   
+
    public CauseSearcherConfig(final TestCase test, final CauseSearcherConfigMixin config) {
-      this(test, !config.isUseNonAggregatedWriter(), 
-            !config.isNotSplitAggregated(), config.getMinTime(), config.isUseCalibrationRun(), !config.isUseEOIs(), 
+      this(test, !config.isUseNonAggregatedWriter(),
+            !config.isNotSplitAggregated(), config.getMinTime(), config.isUseCalibrationRun(), !config.isUseEOIs(),
             config.getStrategy(), config.getLevels());
       this.propertyFolder = config.getPropertyFolder();
    }
 
    public CauseSearcherConfig(final TestCase testCase, final CauseSearcherConfig causeConfig) {
       this(testCase, causeConfig.isUseAggregation(),
-            causeConfig.isSplitAggregated(), causeConfig.getMinTime(), causeConfig.useCalibrationRun(), causeConfig.isIgnoreEOIs(), 
+            causeConfig.isSplitAggregated(), causeConfig.getMinTime(), causeConfig.useCalibrationRun(), causeConfig.isIgnoreEOIs(),
             causeConfig.getRcaStrategy(), causeConfig.getLevels());
    }
 
@@ -86,19 +91,19 @@ public class CauseSearcherConfig implements Serializable {
    public boolean useCalibrationRun() {
       return calibrationRun;
    }
-   
+
    public boolean isIgnoreEOIs() {
       return ignoreEOIs;
    }
-   
+
    public RCAStrategy getRcaStrategy() {
       return rcaStrategy;
    }
-   
+
    public File getPropertyFolder() {
       return propertyFolder;
    }
-   
+
    public int getLevels() {
       return levels;
    }
