04fd1da7348b6964815560d7e1c82d87d2f409d8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-09-26 00:01:47 +0200

    Add test for changed dependency order

--- dependency/src/test/java/de/dagere/peass/dependency/execution/pom/TestExtendDependencies.java
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/pom/TestExtendDependencies.java
@@ -14,7 +14,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 
-public class TestSnapshotRemoveUtil {
+public class TestExtendDependencies {
    @Test
    public void testRegularRemoval() throws FileNotFoundException, IOException, XmlPullParserException {
       File pomFile = new File("src/test/resources/example-snapshot-removal-pom/pom.xml");
@@ -22,26 +22,16 @@ public class TestSnapshotRemoveUtil {
       File testPom = new File("target/pom.xml");
       FileUtils.copyFile(pomFile, testPom);
 
-      SnapshotRemoveUtil.cleanSnapshotDependencies(testPom);
-
       try (FileInputStream inputStream = new FileInputStream(testPom)) {
          final MavenXpp3Reader reader = new MavenXpp3Reader();
          final Model model = reader.read(inputStream);
-         Dependency slf4j = findDependency(model, "slf4j");
-         // Currently, if self groupId is null, all dependencies stay the same
-         Assert.assertEquals("1.0-SNAPSHOT", slf4j.getVersion());
-      }
-   }
 
-   public static Dependency findDependency(final Model model, final String artifactPart) {
-      Dependency slf4j = null;
-      for (Dependency dependency : model.getDependencies()) {
-         
-         if (dependency.getArtifactId().contains(artifactPart)) {
-            slf4j = dependency;
-            
-         }
+         MavenPomUtil.extendDependencies(model, false);
+
+         Dependency kopemeDependency = model.getDependencies().get(0);
+
+         Assert.assertEquals("kopeme-junit", kopemeDependency.getArtifactId());
       }
-      return slf4j;
+
    }
 }
