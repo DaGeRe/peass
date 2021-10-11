3721b63501064fc6dd0c8ec0ab810faad003ea04
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-10-11 16:18:14 +0200

    Fix versions + writing

--- dependency/src/main/java/de/dagere/peass/dependency/execution/gradle/FindDependencyVisitor.java
+++ dependency/src/main/java/de/dagere/peass/dependency/execution/gradle/FindDependencyVisitor.java
@@ -112,7 +112,7 @@ public class FindDependencyVisitor extends CodeVisitorSupport {
    private boolean isJavaPlugin(final String text) {
       if (text.contains("plugin:java") ||
             text.contains("this.id(java)") ||
-            text.contains("his.id(java-library)") || 
+            text.contains("this.id(java-library)") || 
             text.contains("plugin:com.android.library") ||
             text.contains("plugin:com.android.application") ||
             text.contains("com.android.application")) {
