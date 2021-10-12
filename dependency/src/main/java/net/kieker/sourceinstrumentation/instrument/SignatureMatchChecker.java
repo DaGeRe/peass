c10dae4aabb4dde33fc94f7fb8d39f9548b0a239
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-10-12 18:41:53 +0200

    Correctly consider array return type

--- dependency/src/main/java/net/kieker/sourceinstrumentation/instrument/SignatureMatchChecker.java
+++ dependency/src/main/java/net/kieker/sourceinstrumentation/instrument/SignatureMatchChecker.java
@@ -26,36 +26,46 @@ public class SignatureMatchChecker {
       if (includes == null) {
          oneMatches = true;
       } else {
-         for (String pattern : includes) {
-            pattern = fixConstructorPattern(pattern);
-            try {
-               Pattern patternP = PatternParser.parseToPattern(pattern);
-               if (patternP.matcher(signature).matches()) {
-                  oneMatches = true;
-                  break;
-               }
-            } catch (InvalidPatternException e) {
-               LOG.error("Wrong pattern: {}", pattern);
-               throw new RuntimeException(e);
-            }
-         }
+         oneMatches = oneIncludeMatches(signature, oneMatches);
       }
       if (excludes != null) {
-         for (String pattern : excludes) {
-            pattern = fixConstructorPattern(pattern);
-            try {
-               Pattern patternP = PatternParser.parseToPattern(pattern);
-               if (patternP.matcher(signature).matches()) {
-                  oneMatches = false;
-                  break;
-               }
-            } catch (InvalidPatternException e) {
-               LOG.error("Wrong pattern: {}", pattern);
-               throw new RuntimeException(e);
-            }
-         }
+         oneMatches = oneExcludeMatches(signature, oneMatches);
       }
 
+      return oneMatches;
+   }
+
+   private boolean oneExcludeMatches(final String signature, boolean oneMatches) {
+      for (String pattern : excludes) {
+         pattern = fixConstructorPattern(pattern);
+         try {
+            Pattern patternP = PatternParser.parseToPattern(pattern);
+            if (patternP.matcher(signature).matches()) {
+               oneMatches = false;
+               break;
+            }
+         } catch (InvalidPatternException e) {
+            LOG.error("Wrong pattern: {}", pattern);
+            throw new RuntimeException(e);
+         }
+      }
+      return oneMatches;
+   }
+
+   private boolean oneIncludeMatches(final String signature, boolean oneMatches) {
+      for (String pattern : includes) {
+         pattern = fixConstructorPattern(pattern);
+         try {
+            Pattern patternP = PatternParser.parseToPattern(pattern);
+            if (patternP.matcher(signature).matches()) {
+               oneMatches = true;
+               break;
+            }
+         } catch (InvalidPatternException e) {
+            LOG.error("Wrong pattern: {}", pattern);
+            throw new RuntimeException(e);
+         }
+      }
       return oneMatches;
    }
 
