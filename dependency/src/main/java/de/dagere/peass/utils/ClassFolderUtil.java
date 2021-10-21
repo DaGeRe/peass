c35d48f5495252705b8cafdfdc06d756a4d09725
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-10-21 16:15:01 +0200

    Keep order of folders

--- dependency/src/main/java/de/dagere/peass/utils/ClassFolderUtil.java
+++ dependency/src/main/java/de/dagere/peass/utils/ClassFolderUtil.java
@@ -1,10 +1,10 @@
 package de.dagere.peass.utils;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ClassFolderUtil {
-   private static final Set<String> pathes = new HashSet<>();
+   private static final List<String> pathes = new LinkedList<>();
    
    static {
       String[] potentialClassFolders = new String[] { "src/main/java/", "src/test/java/", "src/test/", "src/java/", "src/androidTest/java/" };
@@ -13,7 +13,7 @@ public class ClassFolderUtil {
       }
    }
    
-   public static Set<String> getPathes() {
+   public static List<String> getPathes() {
       return pathes;
    }
    
