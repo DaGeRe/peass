17a9d8356f8a7ad58199cad0e0f1d47437698aca
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-10-24 22:37:02 +0200

    Let test also work if different directory name is used

--- analysis/src/test/java/de/dagere/peass/ci/TestContinuousFolderUtil.java
+++ analysis/src/test/java/de/dagere/peass/ci/TestContinuousFolderUtil.java
@@ -6,9 +6,6 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import de.dagere.peass.ci.ContinuousFolderUtil;
-
-
 /**
  * Tests ContinuousFolderUtil assuming Peass is still in a git repo
  * @author DaGeRe
@@ -16,18 +13,21 @@ import de.dagere.peass.ci.ContinuousFolderUtil;
  */
 public class TestContinuousFolderUtil {
    
+   // This is the repo name, assuming that peass has still its folder structure and this test is in the analysis submodule
+   private String repo_name = new File(".").getAbsoluteFile().getParentFile().getParentFile().getName();
+   
    @Test
    public void testSubFolderPath() throws IOException {
       File test = new File("."); 
       String path = ContinuousFolderUtil.getSubFolderPath(test);
-      Assert.assertEquals("peass" + File.separator + "analysis", path);
+      Assert.assertEquals(repo_name + File.separator + "analysis", path);
    }
    
    @Test
    public void testFolderPath() throws IOException {
       File test = new File(".."); 
       String path = ContinuousFolderUtil.getSubFolderPath(test);
-      Assert.assertEquals("peass", path);
+      Assert.assertEquals(repo_name, path);
    }
    
    
