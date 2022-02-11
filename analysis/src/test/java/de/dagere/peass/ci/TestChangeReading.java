6312dfce87c888d712970e5472a88e8da798313a
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-11 14:23:15 +0100

    Reuse path specification

--- analysis/src/test/java/de/dagere/peass/ci/TestChangeReading.java
+++ analysis/src/test/java/de/dagere/peass/ci/TestChangeReading.java
@@ -19,6 +19,9 @@ import de.dagere.peass.measurement.analysis.ProjectStatistics;
 import de.dagere.peass.vcs.GitCommit;
 
 public class TestChangeReading {
+   
+   public static final File DATA_READING_FOLDER = new File("src/test/resources/dataReading");
+   
    @Test
    public void testReading() throws InterruptedException {
       
@@ -27,11 +30,11 @@ public class TestChangeReading {
       versions.add(new GitCommit("fd79b2039667c09167c721b2823425629fad6d11", "", "", ""));
       VersionComparator.setVersions(versions);
       
-      File baseFolder = new File("src/test/resources/dataReading/android-example-correct");
+      File baseFolder = new File(DATA_READING_FOLDER, "android-example-correct");
       ModuleClassMapping mapping = new ModuleClassMapping(baseFolder, new ProjectModules(new File(baseFolder, "app")), new ExecutionConfig());
       AnalyseFullData afd = new AnalyseFullData(new File("target/test.json"), new ProjectStatistics(), mapping, new StatisticsConfig());
       
-      afd.analyseFolder(new File("src/test/resources/dataReading/measurementsFull/measurements/"));
+      afd.analyseFolder(new File(DATA_READING_FOLDER, "measurementsFull/measurements/"));
       
       ProjectChanges changes = afd.getProjectChanges();
       Set<String> changedClazzes = changes.getVersion("946e4318267b56838aa35da0a2a4e5c0528bfe04").getTestcaseChanges().keySet();
