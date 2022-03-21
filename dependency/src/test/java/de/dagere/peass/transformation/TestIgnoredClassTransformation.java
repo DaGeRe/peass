24175a289e20a1a96ad433a41d5ab1f15a55f763
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-03-21 11:56:34 +0100

    Avoid code duplication

--- dependency/src/test/java/de/dagere/peass/transformation/TestIgnoredClassTransformation.java
+++ dependency/src/test/java/de/dagere/peass/transformation/TestIgnoredClassTransformation.java
@@ -3,7 +3,6 @@ package de.dagere.peass.transformation;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 import org.apache.commons.io.FileUtils;
 import org.hamcrest.MatcherAssert;
@@ -13,12 +12,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import de.dagere.peass.config.MeasurementConfig;
-import de.dagere.peass.dependency.analysis.ModuleClassMapping;
 import de.dagere.peass.dependency.analysis.data.TestCase;
 import de.dagere.peass.dependency.analysis.data.TestSet;
-import de.dagere.peass.execution.utils.ProjectModules;
-import de.dagere.peass.testtransformation.JUnitTestTransformer;
 
 public class TestIgnoredClassTransformation {
 
@@ -38,45 +33,21 @@ public class TestIgnoredClassTransformation {
 
    @Test
    public void testJUnit4Ignore() throws IOException {
-      final File old2 = new File(RESOURCE_FOLDER, "TestClassIgnored.java");
-      testFile = new File(sourcesFolder, "TestClassIgnored.java");
-      FileUtils.copyFile(old2, testFile);
-
-      final JUnitTestTransformer tt = new JUnitTestTransformer(testFolder, MeasurementConfig.DEFAULT);
-      tt.determineVersions(Arrays.asList(new File[] { testFolder }));
-      
-      ProjectModules modules = new ProjectModules(testFolder);
-      TestSet tests = tt.findModuleTests(ModuleClassMapping.SINGLE_MODULE_MAPPING, Arrays.asList(new String[]{""}), modules);
+      TestSet tests = TestIgnoredMethodTransformation.executeTransformation("TestClassIgnored.java", testFolder);
       
       MatcherAssert.assertThat(tests.getTests(), Matchers.not(IsIterableContaining.hasItem(new TestCase("TestClassIgnored#testMe"))));
    }
    
    @Test
    public void testJUnit5Disabled() throws IOException {
-      final File old2 = new File(RESOURCE_FOLDER, "TestClassDisabled.java");
-      testFile = new File(sourcesFolder, "TestClassDisabled.java");
-      FileUtils.copyFile(old2, testFile);
-
-      final JUnitTestTransformer tt = new JUnitTestTransformer(testFolder, MeasurementConfig.DEFAULT);
-      tt.determineVersions(Arrays.asList(new File[] { testFolder }));
-      
-      ProjectModules modules = new ProjectModules(testFolder);
-      TestSet tests = tt.findModuleTests(ModuleClassMapping.SINGLE_MODULE_MAPPING, Arrays.asList(new String[]{""}), modules);
+      TestSet tests = TestIgnoredMethodTransformation.executeTransformation("TestClassDisabled.java", testFolder);
       
       MatcherAssert.assertThat(tests.getTests(), Matchers.not(IsIterableContaining.hasItem(new TestCase("TestClassDisabled#testMe"))));
    }
    
    @Test
    public void testJUnit5KoPeMeIgnore() throws IOException {
-      final File old2 = new File(RESOURCE_FOLDER, "TestClassKoPeMeIgnore.java");
-      testFile = new File(sourcesFolder, "TestClassKoPeMeIgnore.java");
-      FileUtils.copyFile(old2, testFile);
-
-      final JUnitTestTransformer tt = new JUnitTestTransformer(testFolder, MeasurementConfig.DEFAULT);
-      tt.determineVersions(Arrays.asList(new File[] { testFolder }));
-      
-      ProjectModules modules = new ProjectModules(testFolder);
-      TestSet tests = tt.findModuleTests(ModuleClassMapping.SINGLE_MODULE_MAPPING, Arrays.asList(new String[]{""}), modules);
+      TestSet tests = TestIgnoredMethodTransformation.executeTransformation("TestClassKoPeMeIgnore.java", testFolder);
       
       MatcherAssert.assertThat(tests.getTests(), Matchers.not(IsIterableContaining.hasItem(new TestCase("TestClassKoPeMeIgnore#testMe"))));
    }
