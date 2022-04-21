0d924f0d37c702de28ff558398a133b2a6607a6c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-04-21 16:18:17 +0200

    Delete result files that are not read

--- dependency/src/main/java/de/dagere/peass/dependency/traces/OneTraceGenerator.java
+++ dependency/src/main/java/de/dagere/peass/dependency/traces/OneTraceGenerator.java
@@ -98,6 +98,7 @@ public class OneTraceGenerator {
             }
          } else {
             LOG.error("File size exceeds {} MB", kiekerConfig.getTraceSizeInMb());
+            FileUtils.deleteDirectory(kiekerResultFolder);
          }
       }
       if (success) {
