a08a0922d1ba200ac3810bc4abbc7b61f33e211e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-05-26 12:10:36 +0200

    Use temporary folder to fix gh action

--- dependency/src/test/java/de/dagere/peass/dependency/execution/TestGradleReplacement.java
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/TestGradleReplacement.java
@@ -15,16 +15,18 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import de.dagere.kopeme.parsing.GradleParseHelper;
 import de.dagere.peass.config.MeasurementConfig;
-import de.dagere.peass.execution.gradle.GradleTaskAnalyzer;
 import de.dagere.peass.execution.gradle.GradleTestExecutor;
 import de.dagere.peass.execution.gradle.SettingsFileParser;
 import de.dagere.peass.execution.utils.EnvironmentVariables;
 import de.dagere.peass.folders.PeassFolders;
 import de.dagere.peass.testtransformation.JUnitTestTransformer;
+import de.dagere.peass.vcs.VersionControlSystem;
 
 public class TestGradleReplacement {
    
@@ -32,16 +34,22 @@ public class TestGradleReplacement {
 
    private static final String ALTERNATIVE_FILE_CONTENT = "This is an alternative buildfile";
 
+   @TempDir
+   File tempDir;
+   
+   File projectDir;
+   
    @BeforeEach
    public void prepareFolder() throws IOException {
-      FileUtils.deleteDirectory(TestBuildGradle.CURRENT);
-      FileUtils.deleteDirectory(new File(TestBuildGradle.CURRENT.getParentFile(), TestBuildGradle.CURRENT.getName() + "_peass"));
+      FileUtils.deleteDirectory(tempDir);
+      
+      projectDir = new File(tempDir, "project");
 
       File src = new File("src/test/resources/gradle-multimodule-example");
-      FileUtils.copyDirectory(src, TestBuildGradle.CURRENT);
-      GradleTestUtil.initWrapper(TestBuildGradle.CURRENT);
+      FileUtils.copyDirectory(src, projectDir);
+      GradleTestUtil.initWrapper(projectDir);
 
-      List<File> modules = SettingsFileParser.getModules(TestBuildGradle.CURRENT).getModules();
+      List<File> modules = SettingsFileParser.getModules(projectDir).getModules();
       for (File module : modules) {
          File alternativeGradle = new File(module, GradleParseHelper.ALTERNATIVE_NAME);
          LOG.debug("Creating alternative buildfile: {}", alternativeGradle);
@@ -55,7 +63,7 @@ public class TestGradleReplacement {
    public void testReplacement() throws IOException, XmlPullParserException, InterruptedException {
       prepareBuildfiles(true);
 
-      List<File> modules = SettingsFileParser.getModules(TestBuildGradle.CURRENT).getModules();
+      List<File> modules = SettingsFileParser.getModules(projectDir).getModules();
       for (File module : modules) {
          final File gradleFile = GradleParseHelper.findGradleFile(module);
          String fileContent = FileUtils.readFileToString(gradleFile, StandardCharsets.UTF_8);
@@ -67,7 +75,7 @@ public class TestGradleReplacement {
    public void testNoReplacement() throws IOException, XmlPullParserException, InterruptedException {
       prepareBuildfiles(false);
 
-      List<File> modules = SettingsFileParser.getModules(TestBuildGradle.CURRENT).getModules();
+      List<File> modules = SettingsFileParser.getModules(projectDir).getModules();
       for (File module : modules) {
          final File gradleFile = GradleParseHelper.findGradleFile(module);
          String fileContent = FileUtils.readFileToString(gradleFile, StandardCharsets.UTF_8);
@@ -82,12 +90,16 @@ public class TestGradleReplacement {
       config.getExecutionConfig().setUseAlternativeBuildfile(replace);
       Mockito.when(mockedTransformer.getConfig()).thenReturn(config);
 
-      PeassFolders folders = new PeassFolders(TestBuildGradle.CURRENT);
-      Mockito.when(mockedTransformer.getProjectFolder()).thenReturn(TestBuildGradle.CURRENT);
+      PeassFolders folders;
+      try (MockedStatic<VersionControlSystem> utilities = Mockito.mockStatic(VersionControlSystem.class)) {
+         utilities.when(() -> VersionControlSystem.getVersionControlSystem(Mockito.any())).thenReturn(VersionControlSystem.GIT);
+         folders = new PeassFolders(projectDir);
+     }
+      Mockito.when(mockedTransformer.getProjectFolder()).thenReturn(projectDir);
 
       GradleTestExecutor gte = new GradleTestExecutor(folders, mockedTransformer, new EnvironmentVariables());
 
-      gte.prepareKoPeMeExecution(new File(TestBuildGradle.CURRENT, "out.txt"));
+      gte.prepareKoPeMeExecution(new File(projectDir, "out.txt"));
    }
 
 }
