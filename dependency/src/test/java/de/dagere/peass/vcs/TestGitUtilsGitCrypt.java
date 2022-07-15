835da47584d0e52f0555ef387fe79797621ca1b3
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-15 12:39:48 +0200

    Rename TestGitUtils_2 to readable name, disable since it does not work for release environment

--- dependency/src/test/java/de/dagere/peass/vcs/TestGitUtilsGitCrypt.java
+++ dependency/src/test/java/de/dagere/peass/vcs/TestGitUtilsGitCrypt.java
@@ -3,6 +3,7 @@ package de.dagere.peass.vcs;
 import java.io.File;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -13,10 +14,11 @@ import de.dagere.peass.dependencytests.DependencyTestConstants;
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
-public class TestGitUtils_2 {
+public class TestGitUtilsGitCrypt {
 
    @Test
    @DisabledOnOs(OS.WINDOWS)
+   @Disabled
    public void testCheckIsUnlockedWithGitCrypt() throws ZipException {
 
       final String current = DependencyTestConstants.CURRENT.getAbsolutePath();
