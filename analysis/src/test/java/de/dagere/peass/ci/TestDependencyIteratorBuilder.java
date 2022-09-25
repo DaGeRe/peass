5e09c5f8a5cb5e51499607abccbbc43dd3c8f4b2
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-09-25 19:00:57 +0200

    Fix test

--- analysis/src/test/java/de/dagere/peass/ci/TestDependencyIteratorBuilder.java
+++ analysis/src/test/java/de/dagere/peass/ci/TestDependencyIteratorBuilder.java
@@ -19,21 +19,21 @@ public class TestDependencyIteratorBuilder {
    private static final File TEMPORARY_FOLDER = new File("target/temp");
    private static final String SIMPLE_PREDECESSOR = "000001";
    private static final String LAST_RUNNING_VERSION = "00000A";
-   private static final String VERSION_2 = "000002";
+   private static final String COMMIT_2 = "000002";
 
    @Test
    public void testRegularIteratorCreation() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(COMMIT_2);
          
          FixedCommitConfig config = new FixedCommitConfig();
          config.setCommitOld(SIMPLE_PREDECESSOR);
          config.setCommit("HEAD");
 
-         StaticTestSelection dependencies = buildVersionDependencies(LAST_RUNNING_VERSION);
+         StaticTestSelection dependencies = buildCommitDependencies(LAST_RUNNING_VERSION);
          
          CommitIteratorBuilder iterator = new CommitIteratorBuilder(config, dependencies, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, iterator.getCommit());
+         Assert.assertEquals(COMMIT_2, iterator.getCommit());
          Assert.assertEquals(SIMPLE_PREDECESSOR, iterator.getCommitOld());
       }
    }
@@ -41,16 +41,16 @@ public class TestDependencyIteratorBuilder {
    @Test
    public void testNightlyBuildWithoutPrePredecessor() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(COMMIT_2);
          
          FixedCommitConfig config = new FixedCommitConfig();
          config.setCommitOld(null);
          config.setCommit("HEAD");
 
-         StaticTestSelection dependencies = buildVersionDependencies();
+         StaticTestSelection dependencies = buildCommitDependencies();
          
          CommitIteratorBuilder builder = new CommitIteratorBuilder(config, dependencies, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, builder.getCommit());
+         Assert.assertEquals(COMMIT_2, builder.getCommit());
          Assert.assertEquals(null, builder.getCommitOld());
       }
    }
@@ -58,17 +58,17 @@ public class TestDependencyIteratorBuilder {
    @Test
    public void testNightlyBuildWithoutRunningPrePredecessor() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(COMMIT_2);
          
          FixedCommitConfig config = new FixedCommitConfig();
          config.setCommitOld(null);
          config.setCommit("HEAD");
 
-         StaticTestSelection dependencies = buildVersionDependencies(SIMPLE_PREDECESSOR);
+         StaticTestSelection dependencies = buildCommitDependencies(SIMPLE_PREDECESSOR);
          dependencies.getCommits().get(SIMPLE_PREDECESSOR).setRunning(false);
          
          CommitIteratorBuilder builder = new CommitIteratorBuilder(config, dependencies, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, builder.getCommit());
+         Assert.assertEquals(COMMIT_2, builder.getCommit());
          Assert.assertEquals(SIMPLE_PREDECESSOR, builder.getCommitOld());
       }
    }
@@ -76,33 +76,33 @@ public class TestDependencyIteratorBuilder {
    @Test
    public void testNightlyBuildWithPrePredecessor() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(COMMIT_2);
          
          FixedCommitConfig config = new FixedCommitConfig();
          config.setCommitOld(null);
          config.setCommit("HEAD");
 
-         StaticTestSelection dependencies = buildVersionDependencies(SIMPLE_PREDECESSOR, VERSION_2);
+         StaticTestSelection dependencies = buildCommitDependencies(SIMPLE_PREDECESSOR, COMMIT_2);
          
          CommitIteratorBuilder builder = new CommitIteratorBuilder(config, dependencies, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, builder.getCommit());
-         Assert.assertEquals(SIMPLE_PREDECESSOR, builder.getCommitOld());
+         Assert.assertEquals(COMMIT_2, builder.getCommit());
+         Assert.assertNull(builder.getCommitOld());
       }
    }
 
    @Test
    public void testNightlyBuildIteratorCreation() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName(Mockito.any(), Mockito.any())).thenReturn(COMMIT_2);
          
          FixedCommitConfig config = new FixedCommitConfig();
          config.setCommitOld(null);
          config.setCommit("HEAD");
          
-         StaticTestSelection dependencies = buildVersionDependencies(LAST_RUNNING_VERSION);
+         StaticTestSelection dependencies = buildCommitDependencies(LAST_RUNNING_VERSION);
 
          CommitIteratorBuilder iterator = new CommitIteratorBuilder(config, dependencies, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, iterator.getCommit());
+         Assert.assertEquals(COMMIT_2, iterator.getCommit());
          Assert.assertEquals(LAST_RUNNING_VERSION, iterator.getCommitOld());
       }
    }
@@ -110,7 +110,7 @@ public class TestDependencyIteratorBuilder {
    @Test
    public void testInitialRun() {
       try (MockedStatic<GitUtils> gitUtil = Mockito.mockStatic(GitUtils.class)) {
-         gitUtil.when(() -> GitUtils.getName("HEAD", TEMPORARY_FOLDER)).thenReturn(VERSION_2);
+         gitUtil.when(() -> GitUtils.getName("HEAD", TEMPORARY_FOLDER)).thenReturn(COMMIT_2);
          gitUtil.when(() -> GitUtils.getName("HEAD~1", TEMPORARY_FOLDER)).thenReturn(SIMPLE_PREDECESSOR);
          
          FixedCommitConfig config = new FixedCommitConfig();
@@ -118,14 +118,14 @@ public class TestDependencyIteratorBuilder {
          config.setCommit("HEAD");
 
          CommitIteratorBuilder iterator = new CommitIteratorBuilder(config, null, new PeassFolders(TEMPORARY_FOLDER));
-         Assert.assertEquals(VERSION_2, iterator.getCommit());
+         Assert.assertEquals(COMMIT_2, iterator.getCommit());
          Assert.assertEquals(SIMPLE_PREDECESSOR, iterator.getCommitOld());
       }
    }
    
-   private StaticTestSelection buildVersionDependencies(final String... versionNames) {
+   private StaticTestSelection buildCommitDependencies(final String... commitNames) {
       StaticTestSelection dependencies = new StaticTestSelection();
-      for (String versionName : versionNames) {
+      for (String versionName : commitNames) {
          CommitStaticSelection version = new CommitStaticSelection();
          version.setRunning(true);
          dependencies.getCommits().put(versionName, version);
