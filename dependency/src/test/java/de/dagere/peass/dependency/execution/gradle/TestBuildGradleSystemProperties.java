7c7ff7f4c9271bbf965f32e5500f33e6bf1c5875
bam@hbt.de <bam@hbt.de>
2022-09-29 15:24:47 +0200

    Add Test for changing System Property "execution mode"

---
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestBuildGradleSystemProperties.java
@@ -0,0 +1,57 @@
+package de.dagere.peass.dependency.execution.gradle;
+
+import de.dagere.peass.TestConstants;
+import de.dagere.peass.TestUtil;
+import de.dagere.peass.config.MeasurementConfig;
+import de.dagere.peass.dependency.execution.GradleTestUtil;
+import de.dagere.peass.dependency.execution.TestBuildGradle;
+import de.dagere.peass.execution.gradle.GradleBuildfileEditor;
+import de.dagere.peass.execution.utils.EnvironmentVariables;
+import de.dagere.peass.execution.utils.ProjectModules;
+import de.dagere.peass.testtransformation.JUnitTestTransformer;
+import org.apache.commons.io.FileUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class TestBuildGradleSystemProperties {
+
+   private JUnitTestTransformer mockedTransformer;
+
+   @BeforeEach
+   public void setupTransformer() {
+      mockedTransformer = Mockito.mock(JUnitTestTransformer.class);
+      MeasurementConfig config = new MeasurementConfig(2);
+      config.getKiekerConfig().setUseKieker(true);
+      Mockito.when(mockedTransformer.getConfig()).thenReturn(config);
+      Mockito.when(mockedTransformer.getProjectFolder()).thenReturn(TestBuildGradle.CURRENT);
+      Mockito.when(mockedTransformer.getJUnitVersions()).thenReturn(TestConstants.TEST_JUNIT_VERSIONS);
+
+      TestUtil.deleteContents(TestBuildGradle.CURRENT);
+   }
+
+   @Disabled
+   @Test
+   public void testEditSystemPropertiesUnittest() throws IOException {
+      final File gradleFile = new File(TestBuildGradle.GRADLE_BUILDFILE_FOLDER, "buildSystemProperties.gradle");
+
+      final File destFile = GradleTestUtil.initProject(gradleFile, TestBuildGradle.CURRENT);
+
+      GradleBuildfileEditor editor = new GradleBuildfileEditor(mockedTransformer, destFile, new ProjectModules(TestBuildGradle.CURRENT));
+      editor.addDependencies(new File("xyz"), new EnvironmentVariables());
+
+      final String gradleFileContents = FileUtils.readFileToString(destFile, Charset.defaultCharset());
+      System.out.println(gradleFileContents);
+
+      MatcherAssert.assertThat(gradleFileContents, Matchers.not(Matchers.containsString("systemProperty")));
+      MatcherAssert.assertThat(gradleFileContents, Matchers.containsString("'junit.jupiter.execution.parallel.mode.default'        :'SAME_THREAD'"));
+
+   }
+}
