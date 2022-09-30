6c3f008e2f0ccf20c2b423ead7e16bbc21861116
Alex Korn <alex@iotiq.de>
2022-09-30 15:02:21 +0200

    Remove empty line

--- dependency/src/main/java/de/dagere/peass/execution/gradle/GradleBuildfileEditor.java
+++ dependency/src/main/java/de/dagere/peass/execution/gradle/GradleBuildfileEditor.java
@@ -123,7 +123,6 @@ public class GradleBuildfileEditor {
             } else {
                dependencyGradle = "implementation '" + dependency.getGradleDependency() + "'";
             }
-
             visitor.addLine(visitor.getDependencyLine() - 1, dependencyGradle);
          }
          if (testTransformer.getConfig().getExecutionConfig().isUseAnbox()) {
