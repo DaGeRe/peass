b1483d964128d3eff3b9c8a62b12ad628a906c47
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-10-18 15:41:14 +0200

    Clear old results of execution, so twice executability can be checked

--- dependency/src/main/java/de/dagere/peass/dependency/reader/twiceExecution/TwiceExecutableChecker.java
+++ dependency/src/main/java/de/dagere/peass/dependency/reader/twiceExecution/TwiceExecutableChecker.java
@@ -1,10 +1,12 @@
 package de.dagere.peass.dependency.reader.twiceExecution;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -36,25 +38,39 @@ public class TwiceExecutableChecker {
       executionData.addEmptyCommit(commit, predecessor);
 
       executor.prepareKoPeMeExecution(new File(executor.getFolders().getTwiceRunningLogFolder(), "twicePreparation.txt"));
+      truncateLastRunResults();
+      
       for (TestMethodCall testcase : tests) {
          executor.executeTest(testcase, executor.getFolders().getTwiceRunningLogFolder(), transformer.getConfig().getExecutionConfig().getTimeout());
 
-         final Collection<File> folderCandidates = executor.getFolders().findTempClazzFolder(testcase);
-         if (!folderCandidates.isEmpty()) {
-            File folder = folderCandidates.iterator().next();
-            final String methodname = testcase.getMethodWithParams();
-            File oneResultFile = new File(folder, methodname + ".json");
-            Kopemedata data = JSONDataLoader.loadData(oneResultFile);
-            List<VMResult> firstDataCollectorContent = data.getFirstDatacollectorContent();
-            if (firstDataCollectorContent.size() == 1 && firstDataCollectorContent.get(0).isError() == false) {
-               LOG.info("Test is twice executable and therefore likely to be suitable for performance measurement: {}", testcase);
-               executionData.addCall(commit, testcase);
-            } else {
-               LOG.info("Test is *not* twice executable and therefore likely to be *not* suitable for performance measurement: {}", testcase);
-            }
+         checkTestExistance(commit, testcase);
+      }
+   }
+
+   private void truncateLastRunResults() {
+      try {
+         FileUtils.deleteDirectory(executor.getFolders().getTempMeasurementFolder());
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   private void checkTestExistance(String commit, TestMethodCall testcase) {
+      final Collection<File> folderCandidates = executor.getFolders().findTempClazzFolder(testcase);
+      if (!folderCandidates.isEmpty()) {
+         File folder = folderCandidates.iterator().next();
+         final String methodname = testcase.getMethodWithParams();
+         File oneResultFile = new File(folder, methodname + ".json");
+         Kopemedata data = JSONDataLoader.loadData(oneResultFile);
+         List<VMResult> firstDataCollectorContent = data.getFirstDatacollectorContent();
+         if (firstDataCollectorContent.size() == 1 && firstDataCollectorContent.get(0).isError() == false) {
+            LOG.info("Test is twice executable and therefore likely to be suitable for performance measurement: {}", testcase);
+            executionData.addCall(commit, testcase);
          } else {
             LOG.info("Test is *not* twice executable and therefore likely to be *not* suitable for performance measurement: {}", testcase);
          }
+      } else {
+         LOG.info("Test is *not* twice executable and therefore likely to be *not* suitable for performance measurement: {}", testcase);
       }
    }
 }
