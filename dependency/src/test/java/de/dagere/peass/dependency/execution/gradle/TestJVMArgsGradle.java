9a851ec80e7388724f76c6fd76755e6b07b8a9a7
Matti Wilhelmi <maw@hbt.de>
2022-10-26 14:08:25 +0200

    Let testXmxIncreaseSimple use getTestTasks

--- dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
@@ -101,19 +101,12 @@ public class TestJVMArgsGradle {
 
    @Test
    public void testXmxIncreaseSimple() throws IOException {
-      final File gradleFile = new File(GRADLE_BUILDFILE_FOLDER, "buildJVMArgsSimple.gradle");
-
-      final String gradleFileContents = updateGradleFile(gradleFile);
-
-      int testIndex = gradleFileContents.indexOf("test {");
-      int integrationTestIndex = gradleFileContents.indexOf("task integrationTest");
-
-      String testTask = gradleFileContents.substring(testIndex, integrationTestIndex);
+      final String[] testTasks = getTestTasks("buildJVMArgsSimple.gradle");
+      final String testTask = testTasks[0];
+      final String integrationTestTask = testTasks[1];
 
       Assert.assertEquals(1, StringUtils.countMatches(testTask, "jvmArgs"));
       MatcherAssert.assertThat(testTask, Matchers.containsString("-Xmx5g"));
-
-      String integrationTestTask = gradleFileContents.substring(integrationTestIndex);
 
       Assert.assertEquals(1, StringUtils.countMatches(integrationTestTask, "jvmArgs"));
       MatcherAssert.assertThat(integrationTestTask, Matchers.containsString("-Xmx5g"));
