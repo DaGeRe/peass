56c89a7314456c9eb66572eb55aadfca9f84d467
Matti Wilhelmi <maw@hbt.de>
2022-10-26 14:09:33 +0200

    Let testXmxDoubleArgs use getTestTasks

--- dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
@@ -114,19 +114,12 @@ public class TestJVMArgsGradle {
 
    @Test
    public void testXmxDoubleArgs() throws IOException {
-      final File gradleFile = new File(GRADLE_BUILDFILE_FOLDER, "buildJVMDoubleArgs.gradle");
-
-      final String gradleFileContents = updateGradleFile(gradleFile);
-
-      int testIndex = gradleFileContents.indexOf("test {");
-      int integrationTestIndex = gradleFileContents.indexOf("task integrationTest");
-
-      String testTask = gradleFileContents.substring(testIndex, integrationTestIndex);
+      final String[] testTasks = getTestTasks("buildJVMDoubleArgs.gradle");
+      final String testTask = testTasks[0];
+      final String integrationTestTask = testTasks[1];
 
       Assert.assertEquals(1, StringUtils.countMatches(testTask, "jvmArgs"));
       MatcherAssert.assertThat(testTask, Matchers.containsString("-Xmx5g\'"));
-
-      String integrationTestTask = gradleFileContents.substring(integrationTestIndex);
 
       Assert.assertEquals(1, StringUtils.countMatches(integrationTestTask, "jvmArgs"));
       MatcherAssert.assertThat(integrationTestTask, Matchers.containsString("-Xmx5g\'"));
