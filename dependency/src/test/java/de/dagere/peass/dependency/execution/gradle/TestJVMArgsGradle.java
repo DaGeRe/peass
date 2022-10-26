fe9ab0493c7e31c85359d6cf4a3dbe837bfa5562
Matti Wilhelmi <maw@hbt.de>
2022-10-26 14:05:15 +0200

    Let testXmxIncreaseGigabyte use getTestTasks

--- dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
+++ dependency/src/test/java/de/dagere/peass/dependency/execution/gradle/TestJVMArgsGradle.java
@@ -75,18 +75,12 @@ public class TestJVMArgsGradle {
 
    @Test
    public void testXmxIncreaseGigabyte() throws IOException {
-      final File gradleFile = new File(GRADLE_BUILDFILE_FOLDER, "buildJVMArgs.gradle");
+      final String[] testTasks = getTestTasks("buildJVMArgs.gradle");
+      final String testTask = testTasks[0];
+      final String integrationTestTask = testTasks[1];
 
-      final String gradleFileContents = updateGradleFile(gradleFile);
-
-      int testIndex = gradleFileContents.indexOf("test {");
-      int integrationTestIndex = gradleFileContents.indexOf("task integrationTest");
-
-      String testTask = gradleFileContents.substring(testIndex, integrationTestIndex);
       Assert.assertEquals(1, StringUtils.countMatches(testTask, "jvmArgs"));
       MatcherAssert.assertThat(testTask, Matchers.containsString("-Xmx5g"));
-
-      String integrationTestTask = gradleFileContents.substring(integrationTestIndex);
 
       Assert.assertEquals(1, StringUtils.countMatches(integrationTestTask, "jvmArgs"));
       MatcherAssert.assertThat(integrationTestTask, Matchers.containsString("-Xmx5g"));
