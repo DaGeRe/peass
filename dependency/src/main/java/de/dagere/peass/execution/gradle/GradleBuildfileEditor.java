fe8be2d34de64bceb04c1beffbed1c209ec8aefc
Matti Wilhelmi <maw@hbt.de>
2022-11-03 10:54:57 +0100

    Don't call TestTaskParser integrationTestTaskProperties

--- dependency/src/main/java/de/dagere/peass/execution/gradle/GradleBuildfileEditor.java
+++ dependency/src/main/java/de/dagere/peass/execution/gradle/GradleBuildfileEditor.java
@@ -209,23 +209,23 @@ public class GradleBuildfileEditor {
       visitor.addLine(visitor.getLines().size(), "}");
    }
 
-   private void adaptTask(final GradleBuildfileVisitor visitor, final ArgLineBuilder argLineBuilder, TestTaskParser integrationTestTaskProperties, int testTaskLine) {
+   private void adaptTask(final GradleBuildfileVisitor visitor, final ArgLineBuilder argLineBuilder, final TestTaskParser testTaskParser, final int testTaskLine) {
       if (argLineBuilder.getJVMArgs() != null) {
-         if (integrationTestTaskProperties != null && integrationTestTaskProperties.getJvmArgsLine() != -1) {
-            String taskWithoutQuotations = integrationTestTaskProperties.getTestJvmArgsText().substring(1, integrationTestTaskProperties.getTestJvmArgsText().length() - 1);
+         if (testTaskParser != null && testTaskParser.getJvmArgsLine() != -1) {
+            String taskWithoutQuotations = testTaskParser.getTestJvmArgsText().substring(1, testTaskParser.getTestJvmArgsText().length() - 1);
             String testJvmArgsText = "'" + // args should start by ' 
                   taskWithoutQuotations 
                   .replace(", ", ",") // There should be no spaces inside the args
                   .replace(",", "','") // Args should be separated by ','
                   + "'"; // Args should be finished by '
             String adaptedText = argLineBuilder.getJVMArgs(testJvmArgsText);
-            visitor.getLines().set(integrationTestTaskProperties.getJvmArgsLine() - 1, adaptedText);
+            visitor.getLines().set(testTaskParser.getJvmArgsLine() - 1, adaptedText);
          } else {
             visitor.addLine(testTaskLine, argLineBuilder.getJVMArgs());
          }
       }
-      if (integrationTestTaskProperties != null && integrationTestTaskProperties.getMaxHeapSizeLine() != -1 && testTransformer.getConfig().getExecutionConfig().getXmx() != null) {
-         visitor.getLines().set(integrationTestTaskProperties.getMaxHeapSizeLine() -1, "    maxHeapSize = \"" + testTransformer.getConfig().getExecutionConfig().getXmx() + "\"");
+      if (testTaskParser != null && testTaskParser.getMaxHeapSizeLine() != -1 && testTransformer.getConfig().getExecutionConfig().getXmx() != null) {
+         visitor.getLines().set(testTaskParser.getMaxHeapSizeLine() -1, "    maxHeapSize = \"" + testTransformer.getConfig().getExecutionConfig().getXmx() + "\"");
       }
    }
 
