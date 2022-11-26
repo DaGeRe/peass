420bcf9b0987c286aab2c290a461eae4fa727c3a
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-11-26 11:09:24 +0100

    If measurement problems happen inside a test case, it is expected that a NumberTooSmallException occurs - this should not lead to an exception thrown to the outside

--- analysis/src/main/java/de/dagere/peass/visualization/RCAGenerator.java
+++ analysis/src/main/java/de/dagere/peass/visualization/RCAGenerator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.commons.math3.exception.NumberIsTooSmallException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +18,6 @@ import de.dagere.peass.measurement.rca.data.CallTreeNode;
 import de.dagere.peass.measurement.rca.data.CauseSearchData;
 import de.dagere.peass.utils.Constants;
 import de.dagere.peass.visualization.html.HTMLWriter;
-
 
 public class RCAGenerator {
 
@@ -34,7 +34,7 @@ public class RCAGenerator {
       this.source = source;
       this.destFolder = destFolder;
       this.folders = folders;
-      
+
       File details = new File(source.getParentFile(), "details" + File.separator + source.getName());
       data = readData(details);
    }
@@ -49,9 +49,13 @@ public class RCAGenerator {
       preparator.prepare();
       final GraphNode rootNode = preparator.getRootNode();
       TestMethodCall testMethodCall = TestMethodCall.createFromString(data.getTestcase());
-      KoPeMeTreeConverter kopemeTreeConverter = new KoPeMeTreeConverter(folders, data.getMeasurementConfig().getFixedCommitConfig().getCommit(), testMethodCall);
-      HTMLWriter writer = new HTMLWriter(rootNode, data, destFolder, propertyFolder, kopemeTreeConverter.getData());
-      writer.writeHTML();
+      try {
+         KoPeMeTreeConverter kopemeTreeConverter = new KoPeMeTreeConverter(folders, data.getMeasurementConfig().getFixedCommitConfig().getCommit(), testMethodCall);
+         HTMLWriter writer = new HTMLWriter(rootNode, data, destFolder, propertyFolder, kopemeTreeConverter.getData());
+         writer.writeHTML();
+      } catch (NumberIsTooSmallException e) {
+         LOG.error("RCA was not finished successfully - not able to create HTML", e);
+      }
    }
 
    private CauseSearchData readData(final File details) throws IOException, JsonParseException, JsonMappingException {
