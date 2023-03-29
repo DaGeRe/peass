6f4eab827f1d591d233a3cb4bab2ec3a80de5095
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-03-29 11:48:35 +0200

    Move TestCommentRemover, Partially move TestFQNDeterminer

--- dependency/src/test/java/de/dagere/peass/dependency/changesreading/TestFQNDeterminer.java
+++ dependency/src/test/java/de/dagere/peass/dependency/changesreading/TestFQNDeterminer.java
@@ -19,27 +19,6 @@ public class TestFQNDeterminer {
    }
 
    @Test
-   public void testInnerClass() throws FileNotFoundException {
-      File file = new File("src/test/resources/clazzFinderExample/src/main/java/de/dagere/TestMe2.java");
-      CompilationUnit unit = JavaParserProvider.parse(file);
-
-      String fqnTest = FQNDeterminer.getParameterFQN(unit, "TestMe2");
-      Assert.assertEquals("de.TestMe2", fqnTest);
-
-      String fqnInner = FQNDeterminer.getParameterFQN(unit, "Inner");
-      Assert.assertEquals("de.TestMe2$Inner", fqnInner);
-
-      String fqnInnerInner = FQNDeterminer.getParameterFQN(unit, "InnerInner");
-      Assert.assertEquals("de.TestMe2$Inner$InnerInner", fqnInnerInner);
-
-      String fqnEnum = FQNDeterminer.getParameterFQN(unit, "InnerEnum");
-      Assert.assertEquals("de.TestMe2$InnerEnum", fqnEnum);
-
-      String fqnSecond = FQNDeterminer.getParameterFQN(unit, "Second");
-      Assert.assertEquals("de.Second", fqnSecond);
-   }
-
-   @Test
    public void testImportedClass() throws FileNotFoundException {
       
       File dependencyReaderFile = new File("src/main/java/de/dagere/peass/dependency/reader/DependencyReader.java");
