35c3ab2b03a296771d99bb7de18f217962b08ff0
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-01-07 11:47:20 +0100

    Update log4j to 2.22.0

--- dependency/src/main/java/de/dagere/peass/execution/maven/pom/MavenPomUtil.java
+++ dependency/src/main/java/de/dagere/peass/execution/maven/pom/MavenPomUtil.java
@@ -45,7 +45,7 @@ public class MavenPomUtil {
    public static final String COMPILER_PLUGIN_VERSION = "3.12.1";
 
    public static final String JUPITER_VERSION = "5.10.1";
-   public static final String LOG4J_VERSION = "2.21.1";
+   public static final String LOG4J_VERSION = "2.22.0";
 
    private static final Logger LOG = LogManager.getLogger(MavenPomUtil.class);
 
