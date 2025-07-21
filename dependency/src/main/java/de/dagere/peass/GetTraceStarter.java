0f113630a3251761fb376765069e955e3830b5cb
DaGeRe <davidgeorg_reichelt@dagere.de>
2025-07-21 17:31:55 +0200

    Make it possible to get traces

--- dependency/src/main/java/de/dagere/peass/GetTraceStarter.java
+++ dependency/src/main/java/de/dagere/peass/GetTraceStarter.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-import de.dagere.nodeDiffDetector.data.TestMethodCall;
 import de.dagere.peass.config.ExecutionConfig;
 import de.dagere.peass.config.KiekerConfig;
 import de.dagere.peass.config.parameters.ExecutionConfigMixin;
+import de.dagere.peass.dependency.ExecutorCreator;
 import de.dagere.peass.dependency.KiekerResultManager;
+import de.dagere.peass.dependency.RTSTestTransformerBuilder;
+import de.dagere.peass.dependency.TestFinder;
+import de.dagere.peass.dependency.analysis.ModuleClassMapping;
 import de.dagere.peass.dependency.analysis.data.TestSet;
 import de.dagere.peass.dependencyprocessors.CommitComparatorInstance;
 import de.dagere.peass.dependencyprocessors.VersionComparator;
 import de.dagere.peass.execution.utils.EnvironmentVariables;
+import de.dagere.peass.execution.utils.TestExecutor;
 import de.dagere.peass.folders.PeassFolders;
+import de.dagere.peass.testtransformation.TestTransformer;
 import de.dagere.peass.vcs.CommitUtil;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -45,16 +50,23 @@ public class GetTraceStarter implements Callable<Void> {
             executionConfigMixin.isLinearizeHistory());
       VersionComparator.setVersions(commits);
 
-      CommitComparatorInstance comparator = new CommitComparatorInstance(commits);
+//      CommitComparatorInstance comparator = new CommitComparatorInstance(commits);
       
       PeassFolders folders = new PeassFolders(projectFolder);
-      final KiekerResultManager tracereader = new KiekerResultManager(folders, new ExecutionConfig(5), new KiekerConfig(true), new EnvironmentVariables());
-      final TestSet testset = new TestSet();
+      KiekerConfig kiekerConfig = new KiekerConfig(true);
+      ExecutionConfig executionConfig = executionConfigMixin.getExecutionConfig();
+      final KiekerResultManager tracereader = new KiekerResultManager(folders, executionConfig, kiekerConfig, new EnvironmentVariables());
+     
+      TestTransformer transformer = RTSTestTransformerBuilder.createTestTransformer(new PeassFolders(projectFolder), executionConfig, kiekerConfig);
+      TestExecutor executor = ExecutorCreator.createExecutor(folders, transformer, new EnvironmentVariables());
       
-      //TODO Get tests
-      testset.addTest(new TestMethodCall("viewtest.TestMe", "test", ""));
-      tracereader.getExecutor().loadClasses();
-      tracereader.executeKoPeMeKiekerRun(testset, "1", folders.getDependencyLogFolder());
+      final ModuleClassMapping mapping = new ModuleClassMapping(executor);
+      executor.loadClasses();
+      
+      TestFinder testFinder = new TestFinder(executor);
+      TestSet tests = testFinder.findIncludedTests(mapping);
+      
+      tracereader.executeKoPeMeKiekerRun(tests, "1", folders.getDependencyLogFolder());
       return null;
    }
 }
